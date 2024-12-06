b2065b5682faf9a0b6b11d71e0a72ffb508988b7
John Doe <tunglevan050@gmail.com>
2024-12-07 01:06:00 +0700

    haha

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var w = c.width = window.innerWidth,
 		ctx = c.getContext( '2d' ),
 		
 		hw = w / 2, // half-width
-		hh = h / 2 - 180,
+		hh = h / 2 - 160,
 		
 		opts = {
 			strings: [ 'HAPPY', 'BIRTHDAY!'],
