4318bde234df3ac5ca0b05b420019e90a930a252
John Doe <tunglevan050@gmail.com>
2024-12-18 20:44:42 +0700

    hehe

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ ctx.font = opts.charSize + 'px Verdana';
 
 function Letter( char, x, y ){
 	this.char = char;
-	this.x = x+500;
+	this.x = x+100;
 	this.y = y;
 	
 	this.dx = -ctx.measureText( char ).width / 2;
